ca4668d4b8a063801bab44ef839976513abd419c
noa <noaste@gmail.com>
2014-12-18 10:56:02 +0200

    yahgh

--- src/com/myorg/javacourse/NoasServlet.java
+++ src/com/myorg/javacourse/NoasServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class NoasServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+			throws IOException {//yarin
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
